1729f9934833fca0401393f93ac8516466e8dca6
primasalama <inbox.prima@gmail.com>
2017-10-20 10:34:10 +0700

    fix map

--- denah8.php
+++ denah8.php
@@ -16,7 +16,50 @@ ECHO "<FONT SIZE=5><B>$HAK</B>";
 ?>
                   </td>
                 </tr>
-                <tr valign="top"> 
+                <img src="jadi_denah.png" width="1430" height="710" border="0" usemap="#Map" />
+<map name="Map" id="Map">
+  <area shape="poly" coords="53,358" href="#" />
+  <area shape="poly" coords="174,358,176,524,100,500,101,493,83,493,80,468,53,467,50,359" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-1B" />
+  <area shape="poly" coords="109,455" href="" />
+  <area shape="poly" coords="53,177,51,283,174,285,168,121,104,140,102,152,87,152,83,174" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-1" />
+  <area shape="poly" coords="176,117,193,119,193,89,373,87,369,236,176,240" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-2" />
+  <area shape="poly" coords="376,91,378,236,478,236,475,89" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-3" />
+  <area shape="poly" coords="483,90,572,93,569,236,484,236" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-4" />
+  <area shape="poly" coords="790,114,863,218" href="#" />
+  <area shape="poly" coords="795,212,861,215,782,219,783,117,803,113,807,89,860,87,865,209" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-6" />
+  <area shape="poly" coords="923,90,925,177,868,179,866,87" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-7" />
+  <area shape="poly" coords="984,87,987,177,933,181,933,89" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-9" />
+  <area shape="poly" coords="1054,91,1054,179,996,182,989,94" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-10" />
+  <area shape="poly" coords="1115,90,1112,179,1064,179,1059,95" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-11" />
+  <area shape="poly" coords="1193,117,1188,218,1311,216,1308,172,1278,172,1274,149,1255,147,1256,131" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-13" />
+  <area shape="poly" coords="1309,222,1309,277,1192,277,1190,224" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-14" />
+  <area shape="poly" coords="1193,355,1193,493,1194,520,1263,496,1261,485,1280,483,1282,461,1316,457,1312,351" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-10B" />
+  <area shape="poly" coords="1090,422,1093,545,1168,547,1168,520,1190,520,1188,421" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-9B" />
+  <area shape="poly" coords="989,418,993,545,1091,545,1089,423" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-8B" />
+  <area shape="poly" coords="892,420,987,421,984,549,897,545" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-7B" />
+  <area shape="poly" coords="853,461,890,458,892,544,858,545" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=PENTRY-2B2" />
+  <area shape="poly" coords="787,461,849,464,849,548,790,548" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-5B" />
+  <area shape="poly" coords="722,463,723,545,783,548,780,461" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-5B2" />
+  <area shape="poly" coords="581,443,584,549,718,547,719,453" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-5B1" />
+  <area shape="poly" coords="375,358,579,356,579,552,375,549" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-3B" />
+  <area shape="poly" coords="305,444,368,441,369,553,305,553" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-2B3" />
+  <area shape="poly" coords="238,443,301,445,300,551,237,549" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-2B2" />
+  <area shape="poly" coords="177,441,231,441,232,555,198,555,197,524,177,524" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-2B1" />
+  <area shape="poly" coords="421,282,422,350,574,347,572,280" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-R" />
+  <area shape="poly" coords="177,244,180,437,365,437,370,244" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-S1" />
+  <area shape="poly" coords="610,218,778,222,785,420,660,420,655,358,608,351" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-L" />
+  <area shape="poly" coords="989,186,991,413,1190,411,1190,181" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-S2" />
+  <area shape="poly" coords="866,318,984,320,981,380,870,379" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-8" />
+  <area shape="poly" coords="862,221,925,217,924,317,868,321" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-6B" />
+  <area shape="poly" coords="930,219,987,216,984,312,930,309" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-9B" />
+  <area shape="poly" coords="793,262,866,263,868,386,792,385" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-8T" />
+  <area shape="poly" coords="790,219,795,252,858,253,858,224" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-6C" />
+  <area shape="poly" coords="1118,90,1160,90,1163,113,1185,118,1187,181,1122,177" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-12" />
+  <area shape="poly" coords="575,55,647,59,778,54,778,209,720,216,715,147,576,145" href="?HAK=<?php echo $HAK;?>&amp;DENAH=<?php echo $DENAH;?>&amp;KODE=L8-5" />
+  <area shape="poly" coords="109,432" href="#" />
+</map>
+
+                <!-- <tr valign="top"> 
                   <td width="8%" align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-1" title='Ruang Kasubag'><img src="DENAH/G8-1.png" width="100" height="125" border="0"></a></font></td>
                   <td width="13%" align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-2" title='Ruang Rapat Besar'><img src="DENAH/G8-2.png" width="190" height="125"></a></td>
                   <td width="7%" align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-3" title='Ruang Rapat Kecil'><img src="DENAH/G8-3.png" width="85" height="125"></a></td>
@@ -47,11 +90,11 @@ ECHO "<FONT SIZE=5><B>$HAK</B>";
                       </tr>
                     </table>
                     <img src="DENAH/PUTIH.png" width="201" height="50"></td>
-                </tr>
-                <tr valign="top"> 
+                </tr> -->
+                <!-- <tr valign="top"> 
                   <td align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-1B" title='Ruang Sekretaris Deputi'><img src="DENAH/G8-1B.png" width="100" height="150"></a></td>
                   <td align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-2B1" title='Ruang Istirahat'><img src="DENAH/G8-2B1.png" width="60" height="125"></a><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-2B2" title='Ruang Kabag Program'><img src="DENAH/G8-2B2.png" width="60" height="125"></a><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-2B3" title='Ruang Kabag TU'><img src="DENAH/G8-2B3.png" width="60" height="125"></a></td>
-                  <td colspan="2" align="left" valign="top"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-3B" title='Ruang Kerja Deputi'><img src="DENAH/G8-3B.png" width="175" height="150" align="top"></a></td>
+                  <td colspan="2" align="left" valign="top"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-3B" title='Ruang Kerja Deputi'><img src="DENAH/R-Deputi.png" width="160" height="130" align="top" style="margin-top=20px"></a></td>
                   <td align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-5B1" title='Station Room'><img src="DENAH/G8-5B1.png" width="100" height="125"></a><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-5B2" title='Ruang Kabid'><img src="DENAH/G8-5B2.png" width="65" height="115"></a></td>
                   <td align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-6B" title='Ruang Kabid'><img src="DENAH/G8-6B.png" width="65" height="115"></a></td>
                   <td colspan="2" align="left" valign="baseline"><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=PENTRI-2B2" title='Ruang Pentry'><img src="DENAH/PENTRI.png" width="50" height="115"></a><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-7B" title='Ruang Asisten Deputi'><img src="DENAH/G8-7B.png" width="95" height="150"></a><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-8B" title='Ruang Asisten Deputi'><img src="DENAH/G8-8B.png" width="95" height="150"></a><a href="index.php?HAK=<?PHP ECHO"$HAK"?>&DENAH=<?PHP ECHO"$DENAH"?>&KODE=L8-9B" title='Ruang Asisten Deputi'><img src="DENAH/G8-9B.png" width="95" height="150"></a></td>
@@ -65,7 +108,7 @@ ECHO "<FONT SIZE=5><B>$HAK</B>";
                   <td align="left" valign="baseline">&nbsp;</td>
                   <td colspan="2" align="left" valign="baseline">&nbsp;</td>
                   <td align="left" valign="baseline">&nbsp;</td>
-                </tr>
+                </tr> -->
               </table>
               <font size="5" face="Geneva, Arial, Helvetica, sans-serif"><span class="style2"></span></font></td>
           </tr>
